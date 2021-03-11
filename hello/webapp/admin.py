f1dad5048dc1c6f4fe9c8871746eba5ac3a8a12e
Aiganysh <aiganyshx@gmail.com>
2021-03-11 16:49:51 +0600

    imported class Task, Status, Type and return

--- hello/webapp/admin.py
+++ hello/webapp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from webapp.models import Task, Status, Type
+
 
 # Register your models here.
+
+# class TaskAdmin(admin.ModelAdmin):
+#     list_display = ['id', 'status', 'type', 'summary', 'description', 'created_at', 'updated_at']
+#     list_filter = ['status', 'summary']
+#     search_fields = ['status', 'type']
+#     fields = ['id', 'status', 'type', 'summary', 'description', 'created_at', 'updated_at']
+#     readonly_fields = ['created_at', 'updated_at', 'id']
+
+
+admin.site.register(Status)
+admin.site.register(Type)
+admin.site.register(Task)
