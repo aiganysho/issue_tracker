b7920064ef77c87cf5ca98cbddcddba00c1d4b35
Aiganysh <aiganyshx@gmail.com>
2021-03-11 18:20:57 +0600

    Add update and delete view

--- hello/webapp/views.py
+++ hello/webapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import View, TemplateView, RedirectView
 
 from webapp.models import Task
-from webapp.form import TaskForm
+from webapp.form import TaskForm, TaskDeleteForm
 
 # Create your views here.
 
@@ -37,3 +37,42 @@ def task_create_view(request):
             )
             return redirect('view-task', pk=task.id)
         return render(request, 'task_create.html', context={'form': form})
+
+def task_update_view(request, pk):
+
+    task = get_object_or_404(Task, id=pk)
+
+    if request.method == 'GET':
+        form = TaskForm(initial={
+            'summary': task.summary,
+            'description': task.description
+        })
+        return render(request, 'task_update.html', context={'form': form, 'task': task})
+    elif request.method == 'POST':
+        form = TaskForm(data=request.POST)
+        if form.is_valid():
+            task.summary = form.cleaned_data.get('summary'),
+            task.description = form.cleaned_data.get('description')
+            task.save()
+            return redirect('article-view', pk=task.id)
+
+        return render(request, 'task_update.html', context={'form': form, 'task': task })
+
+
+def task_delete_view(request, pk):
+
+    task = get_object_or_404(Task, id=pk)
+
+    if request.method == 'GET':
+        form = TaskDeleteForm()
+        return render(request, 'task_delete.html', context={'task': task, 'form': form})
+    elif request.method == 'POST':
+        form = TaskDeleteForm(data=request.POST)
+        if form.is_valid():
+            if form.cleaned_data['summary'] != task.summary:
+                form.errors['summary'] = ['Названия статей не совпадают']
+                return render(request, 'task_delete.html', context={'task': task, 'form': form})
+
+            task.delete()
+            return redirect('list-task')
+        return render(request, 'task_delete.html', context={'task': task, 'form': form})
