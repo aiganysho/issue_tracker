d494b9363fb0453bac2b12fed69bac71c6ac21a7
Aiganysh <aiganyshx@gmail.com>
2021-03-11 17:45:03 +0600

    Add list task and view task in views.py

--- hello/webapp/views.py
+++ hello/webapp/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import View, TemplateView, RedirectView
+
+from webapp.models import Task
+# from webapp.forms import TaskForm
 
 # Create your views here.
+
+
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs['tasks'] = Task.objects.all()
+        return super().get_context_data(**kwargs)
+
+class TaskView(TemplateView):
+
+    template_name = 'task_view.html'
+
+    def get_context_data(self, **kwargs):
+        kwargs['task'] = get_object_or_404(Task, id=kwargs.get('pk'))
+        return super().get_context_data(**kwargs)
