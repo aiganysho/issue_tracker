5805eb7a77d0f51eefbaf9eb45bb414bee7423f1
Aiganysh <aiganyshx@gmail.com>
2021-03-11 17:43:43 +0600

    write and add urls

--- hello/hello/urls.py
+++ hello/hello/urls.py
@@ -15,7 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webapp.views import (
+    IndexView,
+    TaskView
+)
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', IndexView.as_view(), name='list-task'),
+    path('<int:pk>/', TaskView.as_view(), name='view-task')
 ]
