9f6c9e4af12749b33f40760ac6610ff54bd77893
Aiganysh <aiganyshx@gmail.com>
2021-03-18 16:26:31 +0600

    Changed form.py

--- hello/webapp/form.py
+++ hello/webapp/form.py
@@ -1,16 +1,9 @@
 from django import forms
-
-from webapp.models import Task, Type, Status
+from django.forms import ModelForm
+from webapp.models import Task
 
 
 class TaskForm(forms.ModelForm):
-    type = forms.ModelMultipleChoiceField(queryset=Type.objects.all())
-    status = forms.ModelChoiceField(queryset=Status.objects.all())
-
     class Meta:
         model = Task
         fields = ('summary', 'description', 'type', 'status')
-
-
-class TaskDeleteForm(forms.Form):
-    summary = forms.CharField(max_length=200, required=True, label='Enter name task , to delete!')
