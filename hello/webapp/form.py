8db0673ac0c5706e0007da7779551a8317b836da
Aiganysh <aiganyshx@gmail.com>
2021-03-29 18:29:39 +0600

    Added Project in models.py, form.py,

--- hello/webapp/form.py
+++ hello/webapp/form.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.forms import ModelForm
-from webapp.models import Task
+from webapp.models import Task, Project
 
 
 class TaskForm(forms.ModelForm):
@@ -10,3 +10,8 @@ class TaskForm(forms.ModelForm):
 
 class SearchForm(forms.Form):
     search_value = forms.CharField(max_length=100, required=False, label='Найти')
+
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        fields = ('name', 'description_project', 'start_date', 'end_date')
