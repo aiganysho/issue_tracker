a6efb49b2901fb9b8eb2cfedcac83d0fdb4de9e9
Aiganysh <aiganyshx@gmail.com>
2021-03-29 18:40:14 +0600

    Changed and added some url

--- hello/hello/urls.py
+++ hello/hello/urls.py
@@ -30,12 +30,12 @@ from webapp.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/tracker/', IndexView.as_view(), name='list-task'),
-    path('<int:pk>/tracker/view', TaskView.as_view(), name='view-task'),
+    path('tracker/', IndexView.as_view(), name='list-task'),
+    path('<int:pk>/tracker/view/', TaskView.as_view(), name='view-task'),
     path('<int:pk>/tracker/add/', CreateTask.as_view(), name='add-task'),
-    path('<int:pk>/tracker/update', UpdateTask.as_view(), name='update-task'),
-    path('<int:pk>/tracker/delete', DeleteTask.as_view(), name='delete-task'),
+    path('<int:pk>/tracker/update/', UpdateTask.as_view(), name='update-task'),
+    path('<int:pk>/tracker/delete/', DeleteTask.as_view(), name='delete-task'),
     path('', ProjectView.as_view(), name='list-project'),
     path('<int:pk>/', ProjectDetailView.as_view(), name='view-project'),
-    path('/add', ProjectCreate.as_view(), name='add-project')
+    path('add/', ProjectCreate.as_view(), name='add-project')
 ]
