a3d773e463063694dc26f987bba356646b0f0580
Aiganysh <aiganyshx@gmail.com>
2021-04-12 18:54:51 +0600

    added registration in view and templates

--- hello/accounts/views.py
+++ hello/accounts/views.py
@@ -1,24 +1,49 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from .forms import MyUserCreationForm
 
 
-# Create your views here.
-def login_view(request, *args, **kwargs):
+# # Create your views here.
+# def login_view(request, *args, **kwargs):
+#     context = {}
+#     if request.method == 'POST':
+#         username = request.POST.get('username')
+#         password = request.POST.get('password')
+#
+#         user = authenticate(request, username=username, password=password)
+#         if user is not None:
+#             login(request, user)
+#             return redirect('project:list')
+#         context['has_error'] = True
+#     return render(request, 'registrations/login.html', context=context)
+#
+#
+# @login_required
+# def logout_view(request, *args, **kwargs):
+#     logout(request)
+#     return redirect('project:list')
+#
+# def register_view(request, *args, **kwargs):
+#     if request.method == 'POST':
+#         form = MyUserCreationForm(data=request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             login(request, user)
+#             return redirect('project:list')
+#     else:
+#         form = MyUserCreationForm()
+#     return render(request, 'registrations/user_create.html', context={'form': form})
+
+
+def register_view(request, *args, **kwargs):
     context = {}
+    form = MyUserCreationForm(data=request.POST)
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+        form = MyUserCreationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('project:list')
 
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('list-project')
-        context['has_error'] = True
-    return render(request, 'login.html', context=context)
-
-
-@login_required
-def logout_view(request, *args, **kwargs):
-    logout(request)
-    return redirect('list-project')
+    context['form'] = form
+    return render(request, 'registration/user_create.html', context={'form': form})
