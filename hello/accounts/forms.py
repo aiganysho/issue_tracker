b94438a953b71bf0e7497696e9033488e677cdd3
Aiganysh <aiganyshx@gmail.com>
2021-04-12 18:53:44 +0600

    added forms

---
+++ hello/accounts/forms.py
@@ -0,0 +1,32 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+
+
+
+class MyUserCreationForm(forms.ModelForm):
+    password = forms.CharField(label="Пароль", strip=False, required=True, widget=forms.PasswordInput)
+    password_confirm = forms.CharField(label="Подтвердите пароль", required=True, widget=forms.PasswordInput, strip=False)
+    email = forms.CharField(required=True)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        password_confirm = cleaned_data.get("password_confirm")
+        if password and password_confirm and password != password_confirm:
+            raise forms.ValidationError('Пароли не совпадают!')
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
+        if not first_name and not last_name:
+            raise forms.ValidationError('Заполните одно поле first_name или last_name!')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'password_confirm', 'first_name', 'last_name', 'email']
