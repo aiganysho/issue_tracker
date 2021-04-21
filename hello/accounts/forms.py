b145a5a42bd15d4744310bc8824631bfa47f8045
Aiganysh <aiganyshx@gmail.com>
2021-04-21 17:39:22 +0600

    Реализован редактирование профиля пользователя и юзера

--- hello/accounts/forms.py
+++ hello/accounts/forms.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from accounts.models import Profile
+from django.contrib.auth import get_user_model
 
 
 
@@ -31,8 +32,14 @@ class MyUserCreationForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'password', 'password_confirm', 'first_name', 'last_name', 'email']
-        #
-        # def save(self, commit=True):
-        #     user = super(MyUserCreationForm).save(commit)
-        #     Profile.objects.create(user=user)
-        #     return user
+
+
+class UserChangeForm(forms.ModelForm):
+    class Meta:
+        model = get_user_model()
+        fields = ['first_name', 'last_name', 'email']
+
+class ProfileChangeForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
