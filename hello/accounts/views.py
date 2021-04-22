056c5133b9bc9ce8ad5fa825afa088b742554f8e
Aiganysh <aiganyshx@gmail.com>
2021-04-22 14:55:04 +0600

    Added view UserPasswordChange

--- hello/accounts/views.py
+++ hello/accounts/views.py
@@ -1,42 +1,10 @@
 from django.shortcuts import render, redirect, reverse
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
-from .forms import MyUserCreationForm, UserChangeForm, ProfileChangeForm
+from .forms import MyUserCreationForm, UserChangeForm, ProfileChangeForm, PasswordChangeForm
 from django.views.generic import DetailView, ListView, UpdateView
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, update_session_auth_hash
 from django.core.paginator import Paginator
-from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.contrib.auth.mixins import PermissionRequiredMixin, LoginRequiredMixin
 
-# # Create your views here.
-# def login_view(request, *args, **kwargs):
-#     context = {}
-#     if request.method == 'POST':
-#         username = request.POST.get('username')
-#         password = request.POST.get('password')
-#
-#         user = authenticate(request, username=username, password=password)
-#         if user is not None:
-#             login(request, user)
-#             return redirect('project:list')
-#         context['has_error'] = True
-#     return render(request, 'registrations/login.html', context=context)
-#
-#
-# @login_required
-# def logout_view(request, *args, **kwargs):
-#     logout(request)
-#     return redirect('project:list')
-#
-# def register_view(request, *args, **kwargs):
-#     if request.method == 'POST':
-#         form = MyUserCreationForm(data=request.POST)
-#         if form.is_valid():
-#             user = form.save()
-#             login(request, user)
-#             return redirect('project:list')
-#     else:
-#         form = MyUserCreationForm()
-#     return render(request, 'registrations/user_create.html', context={'form': form})
 
 
 def register_view(request, *args, **kwargs):
@@ -70,13 +38,13 @@ class UserDetailView(DetailView):
         return super().get_context_data(**kwargs)
 
 
-class UserList(ListView):
+class UserList(PermissionRequiredMixin, ListView):
     model = get_user_model()
     template_name = 'user_list.html'
     context_object_name = 'users'
-    permission_required = 'webapp.view_user'
+    permission_required = 'auth.view_user'
 
-class UserChangeView(UpdateView):
+class UserChangeView(LoginRequiredMixin,UpdateView):
     model = get_user_model()
     form_class = UserChangeForm
     template_name = 'user_change.html'
@@ -84,7 +52,7 @@ class UserChangeView(UpdateView):
     profile_form_class = ProfileChangeForm
 
     def get_context_data(self, **kwargs):
-        context = super(UserChangeView, self).get_context_data()
+        context = super(UserChangeView, self).get_context_data(**kwargs)
         context['profile_form'] = kwargs.get('profile_form')
         if context['profile_form'] is None:
             context['profile_form'] = self.get_profile_form()
@@ -98,7 +66,7 @@ class UserChangeView(UpdateView):
             return self.form_valid(user_form, profile_form)
 
     def get_object(self, queryset=None):
-        self.request.user
+        return self.request.user
 
     def form_invalid(self, user_form, profile_form):
         context = self.get_context_data(
@@ -119,6 +87,28 @@ class UserChangeView(UpdateView):
             form_kwargs['files'] = self.request.FILES
         return self.profile_form_class(**form_kwargs)
 
+    def get_success_url(self):
+        return reverse('accounts:user-detail', kwargs={'pk': self.object.pk})
+
+
+class UserPasswordChangeView(LoginRequiredMixin, UpdateView):
+    model = get_user_model()
+    template_name = 'password_change.html'
+    form_class = PasswordChangeForm
+    context_object_name = 'user_obj'
 
     def get_success_url(self):
-        return reverse('accounts:user-detail', kwargs={'pk': object.pk})
+        return reverse('accounts:login')
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+    def form_valid(self, form):
+        response = super(UserPasswordChangeView, self).form_valid(form)
+        update_session_auth_hash(self.request, self.request.user)
+        return response
+
+    def get_success_url(self):
+        return reverse('accounts:user-detail', kwargs={'pk': self.object.pk})
+
+
